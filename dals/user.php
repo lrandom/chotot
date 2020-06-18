0f838a04bdae041f2b71cbd317b3982226587ec4
root <luann4099@gmail.com>
2020-06-18 10:55:12 +0700

    thêm user dal, province dal

--- dals/user.php
+++ dals/user.php
@@ -1,4 +1,5 @@
 <?php 
+  require_once('IModel.php');
   require_once('./../db.php');
   class User extends DB implements IModel
   {
@@ -8,13 +9,69 @@
           parent::__construct();
       }
       
-      public function getAll($page =1,$countRows =10){
+      public function getAll($page = 1,$countRows =10){
          $position = ($page-1)*$countRows;//0
          $query = $this->db->query('SELECT * FROM '.
          $this->tableName.' LIMIT '.$position.','.$countRows);
          return $query->fetchAll();
       }
 
+      public function getOne($id){
+        $query = $this->db->query('SELECT * FROM '
+        .$this->tableName.' WHERE id='.$id);
+        return $query->fetchAll();
+      }
+      
+      public function deleteOne($id){
+        $this->db->query('DELETE FROM '
+        .$this->tableName.' WHERE id ='.$id);
+      }
+
+      public function insertOne($payload){
+        /*username
+        fullname	
+        email
+        phone
+        address	
+        level 
+        0-admin;1-staff;2-bussiness;3 personal	
+        status 0 - deactive, 1- active*/
+
+        $stmn = $this->db->prepare('INSERT INTO '
+        .$this->tableName.'(username,fullname,email,phone,address,level,status) 
+        VALUES(:username, :fullname, :email, :phone, :address,:level,:status)');
+        $stmn->execute(array(
+          ':username'=>$payload['username'],
+          ':fullname'=>$payload['fullname'],
+          ':address'=>$payload['address'],
+          ':phone'=>$payload['phone'],
+          ':email'=>$payload['email'],
+          ':level'=>$payload['level'],
+          ':status'=>$payload['status']
+        ));
+      }
+
+      public function updateOne($payload){
+        $stmn = $this->db->prepare('UPDATE users 
+        SET fullname=:fullname, 
+        email=:email,
+        address=:address
+        phone=:phone,
+        level=:level,
+        status=:status
+         WHERE id = :id');
+        
+        $stmn->execute(
+          array(
+            ':id'=>$payload['id'],
+            ':fullname'=>$payload['fullname'],
+            ':phone'=>$payload['phone'],
+            ':address'=>$payload['address'],
+            ':level'=>$payload['level'],
+            ':status'=>$payload['status']
+          )
+          );
+      }
 
   }
   
